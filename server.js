0f02ec3c1db67df8c0c24ddf8040fb068d16a4c9
juan escobar <jescobar806@gmail.com>
2021-10-13 19:08:13 -0500

    Se adiciona login en el back

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ import Express from 'express';
 import Cors from 'cors';
 import dotenv from 'dotenv';
 import { conectarBD } from './db/db.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 import rutasProducto  from './views/productos/rutas.js'
 import rutasUsuario from './views/usuarios/rutas.js';
 import rutasVenta from './views/ventas/rutas.js';
+
 
 dotenv.config({ path: './.env' });
 
@@ -16,6 +19,21 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://mtictienda.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion-tienda-mintic',
+issuer: 'https://mtictienda.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(rutasProducto);
 app.use(rutasUsuario);
 app.use(rutasVenta);
